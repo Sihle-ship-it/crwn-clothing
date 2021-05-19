c2f9e8ce3e533bf394fa8066f93115b7f0eec588
Q7 <sihle@thedigitalacademy.co.za>
2021-05-19 13:56:17 +0200

    Working on routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
-import Homepage from './pages/homepage/homepage.component';
+import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
